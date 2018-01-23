142b62191b23a6fa395f2c6944c599eeeaf87389
stephenjohnwenceslao <stephenjohnwenceslao@gmail.com>
2018-01-23 21:58:22 +0800

    Update eguide.

--- www/modules/custom/eguide/src/Form/eguideForm.php
+++ www/modules/custom/eguide/src/Form/eguideForm.php
@@ -22,21 +22,55 @@ class eguideForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['destination'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Destination'),
-      '#description' => $this->t('Your destination'),
-      '#maxlength' => 64,
-      '#size' => 64,
+    $uid = \Drupal::currentUser()->id();
+    $query = 0;
+
+    $nid = \Drupal::database()->query("SELECT nfd.nid FROM node_field_data AS nfd
+      LEFT JOIN node__field_user_id AS nfui ON nfui.entity_id = nfd.nid
+      LEFT JOIN node__field_access_status AS nfas ON nfas.entity_id = nfd.nid
+      WHERE nfui.field_user_id_target_id = " . $uid . " AND nfas.field_access_status_value = 'ok'")->fetchField();
+
+    $form['user_id'] = [
+      '#type' => 'hidden',
+      '#default_value' => $uid,
     ];
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#title' => $this->t('Submit'),
-    ];
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Submit'),
-    ];
+
+    $check_python_running = exec("ps -e | grep python");
+    if (!empty($check_python_running)) {
+      $form['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Refresh page'),
+        '#prefix' => '<h2>The system is busy. Please try again later.</h2>'
+      ];
+    }
+    else if (!empty($nid)) {
+      $form['nid'] = [
+        '#type' => 'hidden',
+        '#default_value' => $nid,
+      ];
+      $form['destination'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Destination'),
+        '#description' => $this->t('Your destination'),
+        '#maxlength' => 64,
+        '#size' => 64,
+        '#prefix' => "<center><div id='map_canvas'></div></center>",
+      ];
+      $form['submit'] = [
+        '#type' => 'submit',
+        '#title' => $this->t('Submit'),
+      ];
+      $form['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Submit'),
+      ];
+    }
+    else {
+      $form['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Insert Coin/Bill'),
+      ];
+    }
 
     return $form;
   }
@@ -52,11 +86,17 @@ class eguideForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Display result.
-    foreach ($form_state->getValues() as $key => $value) {
-      drupal_set_message($key . ': ' . $value);
-    }
+    $uid = $form_state->getValue('user_id');
+    $username = \Drupal::database()->query("SELECT name FROM users_field_data WHERE uid = " . $uid)->fetchField();
 
+    if ($form_state->getValue('op') == 'Insert Coin/Bill') {
+      exec("sudo python " . $_SERVER['DOCUMENT_ROOT'] . "/arduino_connect.py " . $username . " " . $uid);
+
+      drupal_set_message("Please insert coin/bill.");
+    }
+    else if ($form_state->getValue('op') == 'Submit') {
+
+    }
   }
 
 }
--- www/themes/server/js/custom.js
+++ www/themes/server/js/custom.js
@@ -1,52 +1,5 @@
 jQuery(function($) {
 	$(document).ready(function() {
-		if ($('.path-frontpage').length) {
-			var created_field = $('input#edit-created[name="created"]');
-			var created_field2 = $('input#edit-created-1[name="created_1"]');
-
-			created_field.datepicker({
-				format: 'mm/dd/yyyy',
-        autoclose: true,
-			});
-
-			created_field2.datepicker({
-				format: 'mm/dd/yyyy',
-        autoclose: true,
-			});
-
-			created_field.keydown(function(e) {
-			   e.preventDefault();
-			   return false;
-			});
-
-			created_field2.keydown(function(e) {
-			   e.preventDefault();
-			   return false;
-			});
-		}
-
-		$(document).ajaxComplete(function() {
-			var count_down = $('span.credit-timer');
-			var body = $('body');
-
-		  if (count_down.length) {
-		  	var count = 20;
-		  	count_down.text(count);
-
-		  	// add overlay
-		  	body.append('<div class="popup_fixed_bg"></div>');
-
-		  	var timer = setInterval(function() {
-
-		  		count_down.text(count);
-
-		  		count -= 1;
-
-		  		if (count == 0) {
-		  			location.reload();
-		  		}
-		  	},1000);
-		  }
-		});
+		
 	});
 });
